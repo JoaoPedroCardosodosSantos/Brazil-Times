9464f2eb63f88dd63ac94f3a2cf63967e6c32e67
rian-junior <rian.juniosantos25@gmail.com>
2025-03-18 18:33:43 -0300

    fix: files, styles, tables

--- config/database.sql
+++ config/database.sql
@@ -10,15 +10,16 @@ CREATE TABLE users (
     username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
-    created_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- hora do post
+    created_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- post time
 );
 
 -- Create the categories table
 CREATE TABLE categories (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL UNIQUE,
+    title VARCHAR(50) NOT NULL UNIQUE,
     description TEXT
 );
+
 
 -- Create the posts table
 CREATE TABLE posts (
@@ -28,12 +29,22 @@ CREATE TABLE posts (
     title VARCHAR(255) NOT NULL,
     content TEXT NOT NULL,
     created_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, -- atualização do tempo
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, -- update time
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (category_id) REFERENCES categories(id)
 );
 
--- Create the comments table
+-- tabela de status do post
+CREATE TABLE status (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    excluidas INT UNIQUE,
+    agendadas INT UNIQUE,
+    rascunho INT UNIQUE,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+
+-- table for coments
 CREATE TABLE comments (
     id INT AUTO_INCREMENT PRIMARY KEY,
     post_id INT NOT NULL,
